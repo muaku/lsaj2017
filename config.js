25142fe096d1f1d5fdd8486a1c33604698d0d733
muaku <m_kaojupor@yahoo.com>
2017-04-15 12:47:33 +0900

    oh2

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-	"mongoDBUri": "",
-	"api": ""
+	"mongoDBUri": "mongodb://heroku_p77j7kdj:o87bcp2lpkhak4gveaadvut4sj@ds155150.mlab.com:55150/heroku_p77j7kdj",
+	"api": "https://lsaj2017.herokuapp.com/register"
 }
